574bd15bbdefae91f9e20f21dd09d40113321e6b
minerva-jupiter <ryouturn@gmail.com>
2025-07-21 16:27:23 +0900

    add permission characters

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<main>
+		<>
 			<h1>Home</h1>
 			<br />
 			<ul>
 				<li><Link href="/password">PasswordGenerator</Link></li>
 			</ul>
-		</main>
+		</>
 	);
 }
--- app/password/page.tsx
+++ app/password/page.tsx
@@ -1,8 +1,35 @@
+'use client'
+
+import "./style-password.css";
+import React, { useState } from "react";
 
 export default function Password() {
+	const [numberIsChecked,setNumberIsChecked] = useState(false);
+	const handleNumberCheckboxChange = (event) => {
+		const checked = event.target.checked;
+		setNumberIsChecked(checked);
+	};
+
+	const [lowletterIsChecked,setLowletterIsChecked] = useState(false);
+	const handleLowletterCheckboxChange = (event) => {
+		const checked = event.target.checked;
+		setLowletterIsChecked(checked);
+	};
+	const [capitalletterIsChecked,setCapitalletterIsChecked] = useState(false);
+	const handleCapitalletterCheckboxChange = (event) => {
+		const checked = event.target.checked;
+		setCapitalletterIsChecked(checked);
+	};
+
 	return (
-		<main>
+		<>
 			<h1>This is Password</h1>
-		</main>
+			<br />
+			<h2>Permission characters</h2>
+			<br />
+			<h4 className="permissionListItem"><input type="checkbox" checked={numberIsChecked} onChange={handleNumberCheckboxChange}/>number</h4>
+			<h4 className="permissionListItem"><input type="checkbox" checked={lowletterIsChecked} onChange={handleLowletterCheckboxChange}/>lowercase letter</h4>
+			<h4 className="permissionListItem"><input type="checkbox" checked={capitalletterIsChecked} onChange={handleCapitalletterCheckboxChange}/>capital letter</h4>
+		</>
 	);
-}
+};
